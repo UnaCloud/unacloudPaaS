bd61199e52524611fcc1e5bdd8f74dada71ab5dc
gasotelo <gasotelo@gmail.com>
2014-06-09 10:43:44 -0500

    asd

--- unacloudpaas/app/unacloud/paas/waiters/CommandWaiter.java
+++ unacloudpaas/app/unacloud/paas/waiters/CommandWaiter.java
@@ -1,10 +1,9 @@
 package unacloud.paas.waiters;
 
-import models.CommandWait;
-import models.Node;
-import models.PlatformExecution;
-import models.RolExecution;
+import models.*;
 import models.enums.ExecutionState;
+import unacloud.paas.back.beans.LogManagerBean;
+import unacloud.paas.back.beans.PlatformExecutionManagerBean;
 import unacloud.paas.monitor.VirtualMachineMonitor;
 
 import java.io.BufferedReader;
@@ -19,63 +18,66 @@ public class CommandWaiter implements Waiter{
          for(Node node : role.getNodes()){
             if(!node.getWaitingCommands().isEmpty()){
                boolean canCheck=false;
-               String h="";
+               String args="";
                for(CommandWait cw : node.getWaitingCommands()){
                   cw.status=ExecutionState.SUCCESS;
-                  h+=" "+cw.getProcessId();
+                  args+=" "+cw.getProcessId();
                }
-               try{
-                  Process p=Runtime.getRuntime().exec("ssh root@"+node.getIpAddress()+" ps "+h);
-                  try(BufferedReader br=new BufferedReader(new InputStreamReader(p.getInputStream()))){
-                     String temp=br.readLine();
-                     if(temp!=null&&temp.contains("PID")&&temp.contains("TTY")){
-                        canCheck=true;
-                     }
-                     for(; (h=br.readLine())!=null;){
-                        String[] j=h.trim().split("\t| +");
-                        if(j[0].matches("[0-9]+")){
-                           Long PID=Long.parseLong(j[0]);
-                           for(CommandWait cw : node.getWaitingCommands()){
-                              if(cw.getProcessId()==PID){
-                                 cw.status=ExecutionState.RUNNING;
-                              }
-                           }
+                try{
+                    Process p=Runtime.getRuntime().exec("ssh root@"+node.getIpAddress()+" ps "+args);
+                    try(BufferedReader br=new BufferedReader(new InputStreamReader(p.getInputStream()))){
+                        String temp=br.readLine();
+                        System.out.println(temp);
+                        if(temp!=null&&temp.contains("PID")&&temp.contains("TTY")){
+                            canCheck=true;
                         }
-                     }
-                  }
-               }catch(IOException ex){
-                   ex.printStackTrace();
-               }
-               for(CommandWait cw : node.getWaitingCommands()){
-                  if(!canCheck){
-                     cw.status=ExecutionState.RUNNING;
-                  }
-                  System.out.println("cw "+cw.getProcessId()+" "+cw.getStatus());
-               }
+                        for(String h; (h=br.readLine())!=null;){
+                            System.out.println(h);
+                            String[] j=h.trim().split("\t| +");
+                            if(j[0].matches("[0-9]+")){
+                                Long PID=Long.parseLong(j[0]);
+                                for(CommandWait cw : node.getWaitingCommands()){
+                                    if(cw.getProcessId()==PID){
+                                    cw.status=ExecutionState.RUNNING;
+                                }
+                                }
+                            }
+                        }
+                    }
+                }catch(IOException ex){
+                    ex.printStackTrace();
+                }
+                for(CommandWait cw : node.getWaitingCommands()){
+                    if(!canCheck){
+                        System.out.println("canCheck "+canCheck);
+                        cw.status=ExecutionState.RUNNING;
+                    }
+                    System.out.println("cw "+cw.getProcessId()+" "+cw.getStatus());
+                }
             }
          }
       }
       boolean complete=true;
-      /*try(Connection con=DatabaseConnection.getConnection(); PreparedStatement ps=con.prepareStatement("update `unacloudpaas`.`commandWait` set `commandWait`.`executionState_state`=? where `commandWait`.`processId`=? and `commandWait`.`idNode`=?;")){
-         for(RoleExecutionEntity role : platform.getRoles()){
-            for(NodeEntity node : role.getNodes()){
-               System.out.println(" "+node.getHostname()+" "+node.getWatingCommands().size());
-               for(CommandWaitEntity cw : node.getWatingCommands()){
-                  if(cw.getState()==ExecutionState.SUCCESS){
-                     PlatformExecutionManager.storeLog(new ExecutionLog(platform.getId(),node.getHostname(),"Command "+cw.getProcessId()+" success"));
-                     ps.setInt(1, ExecutionState.SUCCESS.getId());
+      try/*(Connection con=DatabaseConnection.getConnection(); PreparedStatement ps=con.prepareStatement("update `unacloudpaas`.`commandWait` set `commandWait`.`executionState_state`=? where `commandWait`.`processId`=? and `commandWait`.`idNode`=?;"))*/{
+         for(RolExecution role : platform.rolExecution){
+            for(Node node : role.getNodes()){
+               System.out.println(" "+node.getHostname()+" "+node.waitingCommands.size());
+               for(CommandWait cw : node.waitingCommands){
+                  if(cw.status==ExecutionState.SUCCESS){
+                     LogManagerBean.storeLog(new ExecutionLog(platform.getId(), node.getId(),node.getHostname(), "Command " + cw.getProcessId() + " success"));
+                     /*ps.setInt(1, ExecutionState.SUCCESS.getId());
                      ps.setLong(2, cw.getProcessId());
                      ps.setLong(3, cw.getNodeId());
-                     ps.executeUpdate();
+                     ps.executeUpdate();*/
                   }else{
                      complete=false;
                   }
                }
             }
          }
-      }catch(SQLException ex){
-         Logger.getLogger(VirtualMachineMonitor.class.getName()).log(Level.SEVERE, null, ex);
-      }*/
+      }catch(Exception ex){
+         ex.printStackTrace();
+      }
       return complete;
    }
 }
